4f1bfe45deec791ea07e7065bb13cc1a88032744
amanda <amandafarliana10@gmail.com>
2024-08-12 11:47:55 +0700

    Create Visitor and Librarian entity

--- src/main/webapp/app/entities/entity.routes.ts
+++ src/main/webapp/app/entities/entity.routes.ts
@@ -6,6 +6,16 @@ const routes: Routes = [
     data: { pageTitle: 'Authorities' },
     loadChildren: () => import('./admin/authority/authority.routes'),
   },
+  {
+    path: 'visitor',
+    data: { pageTitle: 'Visitors' },
+    loadChildren: () => import('./visitor/visitor.routes'),
+  },
+  {
+    path: 'librarian',
+    data: { pageTitle: 'Librarians' },
+    loadChildren: () => import('./librarian/librarian.routes'),
+  },
   /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
 ];
 
