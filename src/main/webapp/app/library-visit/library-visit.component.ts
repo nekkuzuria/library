fe20cd4151943cb538affbbc9937f0630d73e579
aminhnh <109867158+aminhnh@users.noreply.github.com>
2024-08-26 05:46:07 +0700

    use PendingTask to show library visits

--- src/main/webapp/app/library-visit/library-visit.component.ts
+++ src/main/webapp/app/library-visit/library-visit.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { LibraryVisitService } from './librrary-visit.service';
 import { IVisitVM } from './library-visit.model';
 import { RouterModule } from '@angular/router';
+import { PendingTaskService } from 'app/pending-task/pending-task.service';
+import { PendingTaskStatus } from 'app/pending-task/pending-task-status.model';
+import { IPendingTaskVM } from 'app/pending-task/pending-task.model';
+import dayjs from 'dayjs/esm';
 
 @Component({
   selector: 'jhi-library-visit',
@@ -11,20 +15,27 @@ import { RouterModule } from '@angular/router';
   styleUrl: './library-visit.component.scss',
 })
 export class LibraryVisitComponent implements OnInit {
-  visits?: IVisitVM[];
+  visits?: IPendingTaskVM[];
+  PendingTaskStatus = PendingTaskStatus;
 
-  constructor(private libraryVisitService: LibraryVisitService) {}
+  constructor(private pendingTaskService: PendingTaskService) {}
 
   ngOnInit(): void {
-    this.loadCurrentLibraryVisit();
+    this.loadCurrentPendingTasks();
   }
-  loadCurrentLibraryVisit(): void {
-    this.libraryVisitService.query().subscribe(response => {
+
+  loadCurrentPendingTasks(): void {
+    this.pendingTaskService.query().subscribe(response => {
       this.visits = response.body || [];
+      this.visits = this.visits.filter(visit => visit.status === PendingTaskStatus.APPROVED);
     });
   }
 
-  trackId(index: number, item: IVisitVM): number {
-    return item.id;
+  trackId(index: number, item: IPendingTaskVM): number {
+    return item.id ?? 0;
+  }
+
+  convertDate(date: dayjs.Dayjs | null | undefined) {
+    return date ? dayjs(date).format('DD/MM/YYYY') : null;
   }
 }
